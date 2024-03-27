1d7b094ce47c9755966437ffee3fb4107a99bfc1
Jakub Janocha <janochajakub02@gmail.com>
2024-03-27 18:58:44 +0100

    BasicData

---
+++ Data/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace Data;
+
+public class Ball
+{
+    private readonly Color _color;
+    private Vector2 _position;
+    private Vector2 _forward;
+    private readonly float _mass;
+    private readonly float _radius;
+
+    public Ball(Color color, Vector2 position, Vector2 forward, float mass, float radius)
+    {
+        _color = color;
+        _position = position;
+        _forward = forward;
+        _mass = mass;
+        _radius = radius;
+    }
+
+    public Color Color => _color;
+
+    public Vector2 Position => _position;
+
+    public Vector2 Forward => _forward;
+
+    public float Mass => _mass;
+
+    public float Radius => _radius;
+}
---
+++ Data/Table.cs
@@ -0,0 +1,21 @@
+﻿namespace Data;
+
+public class Table
+{
+    private float _sizeX;
+    private float _sizeY;
+    private List<Ball> _balls;
+
+    public Table(float sizeX, float sizeY, List<Ball> balls)
+    {
+        _sizeX = sizeX;
+        _sizeY = sizeY;
+        _balls = balls;
+    }
+
+    public float SizeX => _sizeX;
+
+    public float SizeY => _sizeY;
+
+    public List<Ball> Balls => _balls;
+}
