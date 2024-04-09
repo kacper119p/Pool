b0c1e39b521d6e48cabdf65476915d641c82ed6a
kacper119p <kacper119p@gmail.com>
2024-04-09 16:31:16 +0200

    Added Ball Controller

---
+++ Logic/PoolBallController.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+
+namespace Logic;
+
+internal class PoolBallController : IBallController
+{
+    private readonly IBall _ball;
+
+    public PoolBallController(IBall ball, ITable table)
+    {
+        _ball = ball;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        _ball.Position += _ball.Velocity * deltaTime;
+    }
+}
