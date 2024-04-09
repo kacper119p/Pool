eca90579ca2164730096beebe893b02a9b177a4d
kacper119p <kacper119p@gmail.com>
2024-04-09 18:47:44 +0200

    Debug command

---
+++ Presentation/Commands/CreateBallsCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Presentation.Commands;
+
+internal class CreateBallsCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        Debug.WriteLine("a");
+    }
+}
---
+++ Presentation/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Presentation.Commands;
+using System.Windows.Input;
+
+namespace Presentation;
+
+public class ViewModel
+{
+    private CreateBallsCommand _createBallsCommand = new CreateBallsCommand();
+
+    public ICommand CreateBallsCommmand => _createBallsCommand;
+}
