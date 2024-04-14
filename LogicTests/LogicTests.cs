87638bf793fffaef8d11c437c365681eb0f7f925
Jakub Janocha <janochajakub02@gmail.com>
2024-04-14 14:27:10 +0200

    PoolballsBehaviour tests

---
+++ LogicTests/LogicTests.cs
@@ -0,0 +1,23 @@
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Numerics;
+using Data;
+using Logic;
+
+namespace LogicTests
+{
+    public class LogicTests
+    {
+        [Test]
+        public void PoolBallsBehaviourTest()
+        {
+            SimplifiedPoolBallsBehaviour behaviour = new SimplifiedPoolBallsBehaviour();
+            ITable table = new PoolTable(100,100);
+            IBall ball = new PoolBall(Color.Blue, new Vector2(15,15),new Vector2(1,0),1,1 );
+            table.AddBall(ball);
+            behaviour.Tick(1,table);
+            Vector2 support = new Vector2(16, 15);
+            Assert.AreEqual(support,table.Balls[0].Position);
+        }
+    }
+}
