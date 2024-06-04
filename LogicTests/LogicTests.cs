2d1421a63c0eb613c0e886f6503100257a0b52c3
Dorn1 <62305924+Dorn1@users.noreply.github.com>
2024-06-04 20:59:26 +0200

    removed override, that were because there is diffrent ILogger interface xD

--- LogicTests/LogicTests.cs
+++ LogicTests/LogicTests.cs
@@ -79,44 +79,9 @@ namespace LogicTests
             }
         }
 
-        public void Error(string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Error(string message, params object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Warning(string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Warning(string message, params object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Info(string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Info(string message, params object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Debug(string message)
-        {
-            throw new NotImplementedException();
-        }
 
         public void Debug(string message, params object[] args)
         {
-            throw new NotImplementedException();
         }
     }
     
